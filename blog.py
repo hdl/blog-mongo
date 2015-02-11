ee84ac4df8cb87bf61dbfc74f12e7ea622bc3933
mac_vbox <maoze365@gmail.cm>
2015-02-11 00:25:12 -0800

    remove licahost

--- blog.py
+++ blog.py
@@ -417,5 +417,5 @@ sessions = sessionDAO.SessionDAO(database)
 
 
 bottle.debug(True)
-bottle.run(host='localhost', port=8082)         # Start the webserver running and wait for requests
+bottle.run(host='', port=8082)         # Start the webserver running and wait for requests
 
